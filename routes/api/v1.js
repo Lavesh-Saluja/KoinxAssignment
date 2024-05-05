48b5675a8b1b10067b08a121bb7771010773dae7
@Lavesh-Saluja <b521032@iiit-nh.ac.in>
2024-05-05 12:11:36 +0530

    correction in /getBalance route

--- routes/api/v1.js
+++ routes/api/v1.js
@@ -5,12 +5,13 @@ const axios = require('axios');
 const Transaction = require("../../models/transactionSchema");
 require("../../db/connection")
 const AddressSchema = z.string().regex(/^0x[a-fA-F0-9]{40}$/);
-const {getCachedPrice} = require('../../scheduler/etherPriceSchedule')
-router.get("/transactions", async(req, res) => {
+const { getCachedPrice } = require('../../scheduler/etherPriceSchedule')
+
+router.get("/transactions", async (req, res) => {
+
    try{ 
     const address = AddressSchema.parse(req.body.address);
     const url=`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&apikey=${process.env.ETH_SCAN_API_KEY}`
-    
        let result = await axios.get(url);
        result = await result.data.result;
        result = result.map((txn) => {
@@ -23,8 +24,15 @@ router.get("/transactions", async(req, res) => {
         value: txn.value
            }
        });
-        await Transaction.insertMany({user:address,transactions:result});
-       console.log(result);
+       const user = await Transaction.findOne({ user: address });
+
+       if (!user) {
+           await Transaction.insertMany({user:address,transactions:result});
+       }
+       else {
+           await Transaction.updateOne({ user: address }, { transactions: result });
+       }
+        
        //get all the transactions from ++
        res.send(result)
    }
@@ -34,16 +42,20 @@ router.get("/transactions", async(req, res) => {
 
 });
 router.get("/getBalance", async (req, res) => {
-   try{ const address = AddressSchema.parse(req.body.address);
-    const user = await Transaction.findOne({ user: address }).exec();
-    if (!user) {
+    try {
+        const address = AddressSchema.parse(req.body.address);
+        const user = await Transaction.findOne({ user: address }).exec();
+        if (!user) {
         return res.status(404).json({ error: "No transactions found for the user" });
-    }
+        }
 let totalWei = BigInt(0);
        const weiValues = user.transactions; // Example wei values in string format
        let cnt = 0;
-       for (const txn of weiValues) {
-           if (txn.to === address)
+        for (const txn of weiValues) {
+            const txnTo = txn.to.toLowerCase(); // Convert to lowercase for comparison
+            const addressLower = address.toLowerCase(); // Convert to lowercase for comparison
+
+           if (txnTo===addressLower)
            {
                totalWei += BigInt(txn.value);
         }
@@ -53,6 +65,7 @@ let totalWei = BigInt(0);
              totalWei -= BigInt(txn.value);
                }
        }
+       console.log(cnt);
         // Convert total wei to ether
        const totalEther = totalWei.toString();
        // getting ethereum latest price
